6c15bb3861083af3fd89c906899b946fd30a2387
LeoManuel <haiiamleo@gmail.com>
2020-05-30 20:31:08 +0530

    Transact API

---
+++ web-app/servers/routes/transact.js
@@ -0,0 +1,7 @@
+const transactRouter = require('express').Router();
+const controller = require('../controllers/transact.js');
+const authMiddleware = require('../middlewares/auth.js');
+
+authRouter.use('/transact', authMiddleware);
+
+transactRouter.post('/transact', controller.transactProduct);
